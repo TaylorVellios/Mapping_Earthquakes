1a461387159d245776839b824d4dd0e22311cd09
Taylor Vellios <taylorvellios@gmail.com>
2021-05-24 12:38:26 -0500

    13.4.3

--- static/js/logic.js
+++ static/js/logic.js
@@ -32,30 +32,46 @@ let cities = [{
   ];
 console.log('working');
 
+
+
+
+
 let map = L.map('mapid').setView([39.0522, -95.2437], 5);
 
 
-cities.forEach(i => make_marker(i));
+// cities.forEach(i => make_marker(i));
 
-//  Add a marker to the map for Los Angeles, California.
-function make_marker(obj) {
-    L.circleMarker(obj.location, 
-        {
-        radius: obj.population/100000,
-        color: 'orange',
-        fillColor: 'orange',
-        lineweight: 4
-        })
-    .bindPopup("<h2>" + obj.city + ", " + obj.state + "</h2> <hr> <h3> Population: " + obj.population.toLocaleString() + "</h3>")
-    .addTo(map);
-};
+// function make_marker(obj) {
+//     L.circleMarker(obj.location, 
+//         {
+//         radius: obj.population/100000,
+//         color: 'orange',
+//         fillColor: 'orange',
+//         lineweight: 4
+//         })
+//     .bindPopup("<h2>" + obj.city + ", " + obj.state + "</h2> <hr> <h3> Population: " + obj.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// };
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [37.622452,-122.3840716],
+    [30.1934892,-97.6650096],
+    [43.68070602416992,-79.61158752441406],
+    [40.6503694,-73.7563996]
+  ];
 
+  L.polyline(line, {
+      color: 'blue',
+      dashArray: '10,10',
+      weight: 4,
+      opacity: 0.5
+  }).addTo(map);
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY,
-    id: 'mapbox/dark-v10'
+    id: 'light-v10'
 
 });
 
