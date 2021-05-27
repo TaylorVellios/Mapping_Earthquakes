d7154a6f316628edaf9efe08f45097d109ded594
Taylor Vellios <taylorvellios@gmail.com>
2021-05-27 05:58:32 -0500

    init challenge

---
+++ Earthquake_Challenge/static/js/logicStep1.js
@@ -0,0 +1,46 @@
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+    id: 'streets-v11'
+
+});
+
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY,
+        id: 'satellite-streets-v11'
+    
+    });
+
+let baseMaps = {
+    'Streets': streets,
+    'Satellite Streets': satellite
+}
+
+let earthquakes_7days = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(earthquakes_7days).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+        onEachFeature: function(feature,layer) {
+            layer.bindPopup(`<h3>${feature.properties.mag}</h3>`).addTo(map);
+        },
+        color: 'blue',
+        fillColor: 'yellow',
+        weight: 1
+    });
+});
+
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 11,
+    layers: [streets]
+});
+
+
+L.control.layers(baseMaps).addTo(map);
